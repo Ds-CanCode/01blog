d1d28e38728acb84b9629095d46395c73dd703ae
mfadil <fadil.ue2006@gmail.com>
2025-07-13 12:14:10 +0100

    Update

--- front-end/src/app/components/navbar/navbar.component.ts
+++ front-end/src/app/components/navbar/navbar.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatDialogModule } from '@angular/material/dialog';
 import { PopupService } from '../../services/popup.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, MatDialogModule],
+  imports: [RouterModule, MatDialogModule, FormsModule],
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  searchText = '';
   constructor(private popupService: PopupService) { }
 
   openLogin() {
